1a3d7f92ece9bbed316a4f4f6aa93d8f82d87ea3
Alex <57764940+axnetg@users.noreply.github.com>
2022-12-04 16:27:28 +0100

    2022 Day 4

---
+++ Y2022/Day04/Solution.cs
@@ -0,0 +1,41 @@
+﻿using AdventOfCode.Core;
+using System.Diagnostics;
+
+namespace AdventOfCode.Y2022.Day04;
+
+[Challenge("Camp Cleanup", 2022, 4)]
+public class Solution : ISolution
+{
+    public object PartOne(string input)
+    {
+        /* In how many pairs does one range fully contain the other? */
+        return GetCleaningAssignments(input)
+            .Count(c => c[0].All(elem => c[1].Contains(elem)) || c[1].All(elem => c[0].Contains(elem)));
+    }
+
+    public object PartTwo(string input)
+    {
+        /* In how many pairs do the ranges overlap? */
+        return GetCleaningAssignments(input)
+            .Count(c => c[0].Intersect(c[1]).Any());
+    }
+
+    private IEnumerable<int[][]> GetCleaningAssignments(string input)
+    {
+        return input.Split("\n")
+            .Select(pair => pair.Split(","))
+            .Select(cleanup => cleanup.Select(c =>
+                {
+                    var roomRanges = c.Split("-").Select(r => int.Parse(r)).ToArray();
+                    return SequenceRange(roomRanges[0], roomRanges[1]).ToArray();
+                })
+                .ToArray()
+            );
+    }
+
+    private IEnumerable<int> SequenceRange(int start, int end)
+    {
+        Debug.Assert(start <= end);
+        return Enumerable.Range(start, end - start + 1);
+    }
+}
