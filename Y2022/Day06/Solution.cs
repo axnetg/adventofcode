7d0dfa21bb8d1c0f7ecafe25820c23827e361e90
Alex <57764940+axnetg@users.noreply.github.com>
2022-12-18 00:45:54 +0100

    2022 Day 6

---
+++ Y2022/Day06/Solution.cs
@@ -0,0 +1,25 @@
+﻿using AdventOfCode.Core;
+
+namespace AdventOfCode.Y2022.Day06;
+
+[Challenge("Tuning Trouble", 2022, 6)]
+public class Solution : ISolution
+{
+    public object PartOne(string input) => GetMarkerIndex(input, 4);
+
+    public object PartTwo(string input) => GetMarkerIndex(input, 14);
+
+    private int GetMarkerIndex(string data, int packetSize)
+    {
+        for (int i = 0; i < data.Length - packetSize; i++)
+        {
+            string chars = data.Substring(i, packetSize);
+            if (chars.Distinct().Count() == packetSize)
+            {
+                return i + packetSize;
+            }
+        }
+
+        throw new NotSupportedException();
+    }
+}
