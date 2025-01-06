4f146925ed6845c975ee5bea5d91fab77b845d0c
Alex <57764940+axnetg@users.noreply.github.com>
2025-01-06 21:33:08 +0100

    feat: 2024 Day 25 implementation

---
+++ Y2024/Day25.cs
@@ -0,0 +1,48 @@
+﻿namespace Axnetg.AdventOfCode.Y2024;
+
+[Puzzle("Code Chronicle", 2024, 25)]
+public sealed class Day25 : IPuzzle
+{
+    private readonly IEnumerable<Block> _blocks;
+
+    public Day25(TextReader reader)
+    {
+        _blocks = reader.ReadToEnd()
+            .ReplaceLineEndings("\n")
+            .Split("\n\n")
+            .Select(x => new Block(x));
+    }
+
+    public PuzzleResult SolvePartOne()
+    {
+        return GetAllCombinations().Count(x => AreComplementaryBlocks(x.First, x.Second));
+    }
+
+    public PuzzleResult SolvePartTwo()
+    {
+        return "N/A";
+    }
+
+    private IEnumerable<(Block First, Block Second)> GetAllCombinations()
+    {
+        var list = _blocks.ToList();
+
+        for (int i = 0; i < list.Count; i++)
+        {
+            for (int j = i + 1; j < list.Count; j++)
+            {
+                yield return (list[i], list[j]);
+            }
+        }
+    }
+
+    private static bool AreComplementaryBlocks(Block b1, Block b2)
+    {
+        // We define complementary blocks as those that, for each position of the grid,
+        // never overlap (this is, both must never be '#' at the same time).
+
+        return b1.Lines.Zip(b2.Lines).All(pair => pair is not ('#', '#'));
+    }
+
+    private sealed record Block(string Lines);
+}
